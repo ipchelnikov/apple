b2e7061f0a75b1988b961e8101a6aa29b3b23914
craft <craft@crafts-Mac-Pro.local>
2016-07-31 15:53:25 +0300

    Hello Word!

---
+++ hello_world/hello_world/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello_world
+//
+//  Created by craft on 7/31/16.
+//  Copyright (c) 2016 craft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
